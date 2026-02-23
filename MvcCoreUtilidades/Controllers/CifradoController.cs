3157649866d74aa085166b837cef3ccbf3506121
MartaQMP <martaquirosmp@gmail.com>
2026-02-23 12:57:44 +0100

    23/02 dia

---
+++ MvcCoreUtilidades/Controllers/CifradoController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreUtilidades.Helpers;
+
+namespace MvcCoreUtilidades.Controllers
+{
+    public class CifradoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult CifradoBasico()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CifradoBasico(string contenido, string resultado, string accion)
+        {
+            // CIFRAMOS EL CONTENIDO
+            string response = HelperCryptography.EncriptarTextoBasico(contenido);
+            if(accion.ToLower() == "cifrar")
+            {
+                ViewBag.TextoCifrado = response;
+            }
+            else if(accion.ToLower() == "comparar")
+            {
+                // SI EL USUARIO QUIERE COMPARAR NOS ESTARA ENVIANDO EL TEXTO A COMPARAR EN resultado
+                if(response != resultado)
+                {
+                    ViewBag.Mensaje = "Los datos no coinciden";
+                }else
+                {
+                    ViewBag.Mensaje = "Todo correcto";
+                }
+            }
+                return View();
+        }
+
+
+        public IActionResult CifradoEficiente()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CifradoEficiente(string contenido, string resultado, string accion)
+        {
+            if(accion.ToLower() == "cifrar")
+            {
+                string response = HelperCryptography.CifrarContenido(contenido, false);
+                ViewBag.TextoCifrado = response;
+                ViewBag.Salt = HelperCryptography.Salt;
+            }
+            else if(accion.ToLower() == "comparar")
+            {
+                string response = HelperCryptography.CifrarContenido(contenido, true);
+                if (response != resultado)
+                {
+                    ViewBag.Mensaje = "Los datos no coinciden";
+                }else
+                {
+                    ViewBag.Mensaje = "Todo correcto";
+                }
+            }
+                return View();
+        }
+    }
+}
---
+++ MvcCoreUtilidades/Controllers/MailsController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Net.Mail;
+
+namespace MvcCoreUtilidades.Controllers
+{
+    public class MailsController : Controller
+    {
+        private IConfiguration configuration;
+
+        public MailsController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public IActionResult SendMail()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SendMail(string to, string asunto, string mensaje)
+        {
+            string user = this.configuration.GetValue<string>("MailSettings:Credentials:User");
+            // OBJETO PARA LA INFORMACION DEL MAIL
+            var mail = new MailMessage();
+            mail.From = new MailAddress(user);
+            mail.To.Add(to);
+            mail.Subject = asunto;
+            mail.Body = mensaje;
+            mail.IsBodyHtml = true;
+            mail.Priority = MailPriority.Normal;
+            // RECUPERAMOS LOS DATOS PARA EL OBJETO QUE MANDA EL PROPIO MAIL
+            string password = this.configuration.GetValue<string>("MailSettings:Credentials:Password");
+            string host = this.configuration.GetValue<string>("MailSettings:Server:Host");
+            int port = this.configuration.GetValue<int>("MailSettings:Server:Port");
+            bool ssl = this.configuration.GetValue<bool>("MailSettings:Server:Ssl");
+            bool defaultCredentials = this.configuration.GetValue<bool>("MailSettings:Server:DefaultCrendentials");
+            SmtpClient client = new SmtpClient
+            {
+                Host = host,
+                Port = port,
+                EnableSsl = ssl,
+                UseDefaultCredentials = defaultCredentials
+            };
+            NetworkCredential credential = new NetworkCredential(user, password);
+            client.Credentials = credential;
+            await client.SendMailAsync(mail);
+
+            ViewBag.Mensaje = "Mensaje enviado correctamente";
+            return View();
+        }
+    }
+}
